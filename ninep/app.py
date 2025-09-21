f9234f340e2f9e647753a73bb9d93013b077431f
sahithibilla192004 <sahithibilla0@gmail.com>
2025-09-21 13:37:27 +0530

    Add restored ninep folder with contents

---
+++ ninep/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app, path="/metrics")
+
+@app.route("/hello")
+def hello():
+    return jsonify(message="Hello from monitored app!")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
